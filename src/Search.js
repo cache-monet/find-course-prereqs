5af11394abdd0cab8cc23537e948979fd7a058ab
crake <remiaston@gmail.com>
2018-09-03 23:31:10 -0400

    updated to use async await

--- src/Search.js
+++ src/Search.js
@@ -5,6 +5,10 @@ import axios from 'axios'
 const API_URL = 'https://api.uwaterloo.ca/v2';
 const API_KEY = 'd62eb4cf89a63f8f74cc4b336b544df3';
 
+async function loadData(address) {
+  return axios.get(`${API_URL}/${address}.json?key=${API_KEY}`);
+}
+
 class Search extends Component {
   constructor(props) {
     super(props);
@@ -12,50 +16,51 @@ class Search extends Component {
       subjects: [], courses: [], prereqs: {}, subject: {}, course: {}
     };
   }
-  getSubjects = () => {
-    axios.get(`${API_URL}/codes/subjects.json?key=${API_KEY}`)
-    .then(({ data }) => {
-      this.setState({
-        subjects: data.data.map(d => {
-          return ({ value: d.subject, label: d.subject })
-        })
-      })
-    })
-  };
-  getCourses = () => {
-    axios.get(`${API_URL}/courses/${this.state.subject.value}.json?key=${API_KEY}`)
-    .then(({ data }) => {
-      this.setState({
-        courses: data.data.map(d => {
-          return ({value: d.catalog_number, label: d.catalog_number});
-        })
+
+  getSubjects = async () => {
+    const resp = await loadData('codes/subjects')
+    this.setState({
+      subjects: resp.data.data.map(d =>{
+        return ({ value: d.subject, label: d.subject})
       })
     })
   };
 
-  getPrereqs= () => {
-    axios.get(`${API_URL}/courses/${this.state.subject.value}/${this.state.course.value}.json?key=${API_KEY}`)
-    .then(({ data }) => {
-      this.setState({
-       prereqs: data.data
+  getCourses = async () => {
+    const resp = await loadData(`courses/${this.state.subject.value}`);
+    this.setState({
+      courses: resp.data.data.map(d => {
+        return ({ value: d.catalog_number, label: d.catalog_number });
       })
     })
-
   };
+
+  getPrereqs = async () => {
+    const resp = await loadData(`courses/${this.state.subject.value}/${this.state.course.value}`)
+    this.setState({
+      prereqs: resp.data.data
+    })
+  };
+
   selectSubject = subject => {
     this.setState({
       subject: subject
     }, () => {
-      this.state.course = {};
-      this.state.prereqs = {};
+      this.setState({
+        course: {},
+        prereqs: {}
+      });
       this.getCourses();
     });
   }
+
   selectCourse = course => {
     this.setState({
       course: course
     }, () => {
-      this.state.prereqs = {};
+      this.setState({
+        prereqs:  {}
+      });
       this.getPrereqs();
     });
   }
@@ -67,20 +72,21 @@ class Search extends Component {
   render() {
     return (
       <div className="ClassSearch">
+        <p>Select Subject</p>
         <Select
-          placeholder='Select Subject'
           className="SubjectSelect"
           options={this.state.subjects}
           value={this.state.subject}
           onChange={this.selectSubject}
         />
+        <p>Select Course</p>
         <Select
-          placeholder='Select Course'
           className="CourseSelect"
           options={this.state.courses}
           value={this.state.course}
           onChange={this.selectCourse}
         />
+        <h4>Prerequisites:</h4>
         <p>{this.state.prereqs.prerequisites}</p>
       </div>
     );
